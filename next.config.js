f9daa405c64701cdd18095fef534e72924dc9bab
DD155 <31744748+DD155@users.noreply.github.com>
2023-11-01 13:20:17 -0400

    tested swr for getting data from last fm REST calls. need to move this to a server side location to obscure the api key later

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     appDir: true,
+    serverRuntimeConfig: {
+        lfmKey: process.env.LFM_API, // Pass through env variables
+    }   
 }
 
 module.exports = nextConfig
--- src/app/visualize/page.tsx
+++ src/app/visualize/page.tsx
@@ -1,15 +1,40 @@
 'use client'
 
+import useSWR from 'swr'
 import { useSearchParams } from "next/navigation"
 import { useEffect } from "react"
 
+
 const VisualizePage = () => {
+    const url = process.env.NEXT_PUBLIC_API_URL // https://ws.audioscrobbler.com/2.0/
     const searchParams = useSearchParams()
     const username:string = searchParams.get("username") ? searchParams.get("username") as string : "" // null check
+    //const [userInfo, setUserInfo] = useState<>()
+    
+    // fetcher for last fm endpoint https://ws.audioscrobbler.com/2.0/
+    const userFetcher = async ([url, user]:string[]) => 
+        await fetch(`${url}&user=${user}&api_key=${process.env.LFM_API}&format=json`)
+            .then((res) => res.json())
+    
 
-    useEffect(() => {
-        console.log(username)
-    }, [])
+    const { data, error, isLoading } = useSWR([`${url}?method=user.getinfo`, username], userFetcher)
+
+    // useEffect(() => {
+    //     console.log(username)
+    // }, [])
+
+    if (error) {
+        return (<>
+            <p> Request failed </p>
+        </>)
+    }
+
+    if (isLoading) return <p> Data loading </p>
+    
+    return <>
+        <p> Username: {data.user.name}</p>
+        <p> Total artists: {data.user.artist_count} </p>
+    </>
 }
 
 export default VisualizePage
