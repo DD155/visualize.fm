abe71077718880b2f9f5523dac83fae98c0dd772
DD155 <31744748+DD155@users.noreply.github.com>
2023-11-07 12:56:51 -0500

    changed search bar to handle click on enter, continued tailwind and css learning

--- src/app/components/Searchbar/page.tsx
+++ src/app/components/Searchbar/page.tsx
@@ -11,19 +11,33 @@ const Searchbar = () => {
         setUsername(e.target.value)
     }
 
-    const searchOnclick = () => {
-        router.push(`/visualize?username=${username}`)  // pass the username as a query
+    const searchOnclick = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        if (username.length > 0)
+            router.push(`/visualize?username=${username}`)  // pass the username as a query
     }
 
     return (
         <>
             <div className='h-12'>
-                <input className='p-2 w-1/4 h-full' 
-                    type='text' placeholder="Enter a username..."  onChange={inputHandler}/>
-                <button 
-                    className='hover:bg-red p-2 text-s h-full bg-main-red text-white'
-                    onClick={searchOnclick}> Search 
-                </button>
+                <form onSubmit={searchOnclick} className='h-full flex flex-row p-1 justify-center'>
+                    <input className='p-2 w-1/4 h-full' 
+                        type='text' placeholder="Enter a username..."  onChange={inputHandler}/>
+                    
+                    <button 
+                        type='submit'
+                        className='hover:bg-red-700 p-2 text-s h-full bg-main-red text-white'> 
+                        Search 
+                    </button>
+                    
+                    {/*
+                    <input 
+                        type='image'
+                        className='bg-white bg-[url("/next.svg")] bg-cover w-8 h-full' alt=''> 
+                        
+                    </input>
+                    */}
+                </form>
             </div>
             
         </>
