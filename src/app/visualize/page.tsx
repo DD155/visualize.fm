ad7233f99a69b8b4198c0e208a9f26999bbaec18
DD155 <31744748+DD155@users.noreply.github.com>
2023-11-08 12:16:35 -0500

    added error and loading messages. created gradient animation for loading and added spinner to it. need to figure out best way to display it

--- src/app/visualize/page.tsx
+++ src/app/visualize/page.tsx
@@ -44,11 +44,24 @@ const VisualizePage = () => {
 
     if (error) {
         return (<>
-            <p> Request failed </p>
+        <div className='h-screen flex justify-center items-center'>
+            <p className='text-white text-4xl'> Couldn't retrieve data...</p>
+        </div>
         </>)
     }
 
-    if (isLoading) return <p> Data loading </p>    
+    if (isLoading) {
+        return (
+        <div className='h-screen flex justify-center items-center'>
+            <p className='animate-text bg-gradient-to-r from-white via-red-200 to-main-red bg-clip-text text-transparent  text-4xl'> Visualizing your music... </p>
+            <svg className="animate-spin ml-3 mr-3 h-8 w-8 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
+                <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+            </svg>
+        </div>
+        )    
+
+    }
 
     return <>
         {userInfo.profilePic && 
